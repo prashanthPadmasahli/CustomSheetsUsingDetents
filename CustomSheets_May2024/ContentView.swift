881c765d2faef3f273cebcaae7f22f11fa432731
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-05-31 16:36:17 +0530

    Initial Commit

---
+++ CustomSheets_May2024/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CustomSheets_May2024
+//
+//  Created by mac on 31/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomSheets_May2024/CustomSheets_May2024App.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSheets_May2024App.swift
+//  CustomSheets_May2024
+//
+//  Created by mac on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomSheets_May2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
